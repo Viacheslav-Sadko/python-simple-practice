9ef269c223637e750a5d7d060cafd4b3ce0fb6a8
Viacheslav <73247577+Viacheslav-Sadko@users.noreply.github.com>
2024-11-27 12:30:33 +0200

    With class

--- gui.py
+++ gui.py
@@ -1,14 +1,21 @@
 from tkinter import *
 
-width = 600
-height = 300
-x = 400
-y = 200
+app_width = 600
+app_height = 300
+app_x = 400
+app_y = 200
 
-window = Tk()
-window.title("My app")
-window.geometry(f"{width}x{height}+{x}+{y}")
-window.resizable(False, False)
-window.iconbitmap("icon.ico")
+class Window():
+    def __init__(self, width = app_width, height = app_height, x = app_x, y = app_y, title = "Window", resizable = (False, False), icon = "icon.ico"):
+        self.root = Tk()
+        self.root.title(title)
+        self.root.geometry(f"{width}x{height}+{x}+{y}")
+        self.root.resizable(resizable[0], resizable[1])
+        if icon:
+            self.root.iconbitmap(icon)
 
-window.mainloop()
+    def run(self):
+        self.root.mainloop()
+
+wind = Window()
+wind.run()
